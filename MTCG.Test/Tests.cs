b7fe4fbbe6563f8e13973d564f216cfef1a2045c
HP_ece\Ece <if23b062@technikum-wien.at>
2025-01-30 17:43:57 +0100

    unit test implementation

---
+++ MTCG.Test/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Win32;
+using NUnit.Framework;
+using MTCG.Server;
+
+namespace MTCG.Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+    }
+}
